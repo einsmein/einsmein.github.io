5770c9d99a94fc84359d08cc479cc1b8f1ffe09a
einsmein <einsmein@hotmail.com>
2018-11-16 11:29:30 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
         $(window).scroll( function ()
         {
-              var navHeight = $( window ).height() - 70;
+//               var navHeight = $( window ).height() - 70;
+              var navHieght = 110;
               if ($(window).scrollTop() > navHeight) {
                 $('nav').addClass('fixed');
               }
