27767ee37dedc425db074d7777bd4808469b94b5
einsmein <einsmein@hotmail.com>
2018-11-16 10:40:55 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $().ready(function() {
-   $("#text").html("Text added by jQuery code.");
+   $(window).bind('scroll', function() {
+      var navHeight = $( window ).height() - 70;
+         if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+			}
+			else {
+				$('nav').removeClass('fixed');
+			}
+		});
+	});
 });
