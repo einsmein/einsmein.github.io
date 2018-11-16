7845d411a73b304bc7a0442556cb96e54bad98aa
einsmein <einsmein@hotmail.com>
2018-11-16 10:58:52 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$().ready(function() {
+$(document).ready(function() {
 	$(window).bind('scroll', function() {
 		var navHeight = $( window ).height() - 70;
 		if ($(window).scrollTop() > navHeight) {
