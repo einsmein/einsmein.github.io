aaa06fbfb3ec14b90435651612f118e6457031ca
einsmein <einsmein@hotmail.com>
2018-11-16 10:52:50 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 $().ready(function() {
-   $(window).bind('scroll', function() {
-      var navHeight = $( window ).height() - 70;
-         if ($(window).scrollTop() > navHeight) {
-            $('nav').addClass('fixed');
-			}
-			else {
+	$(window).bind('scroll', function() {
+		var navHeight = $( window ).height() - 70;
+		if ($(window).scrollTop() > navHeight) {
+			$('nav').addClass('fixed');
+		}
+		else {
 				$('nav').removeClass('fixed');
-			}
-		});
+		}
 	});
 });
